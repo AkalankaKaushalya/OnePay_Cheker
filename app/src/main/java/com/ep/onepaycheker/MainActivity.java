740759fa640b97e8db5dd16ea97f2dd52a969187
Akalanka Kaushalya <akalankakaushalya343@gmail.com>
2023-08-16 13:59:06 +0530

    not work resalt

--- app/src/main/java/com/ep/onepaycheker/MainActivity.java
+++ app/src/main/java/com/ep/onepaycheker/MainActivity.java
@@ -30,17 +30,30 @@ import lk.onepay.android.ipg.utilities.OnepayIPG;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+
+    private static final String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    public static String generateRandomString() {
+        SecureRandom random = new SecureRandom();
+        int length = random.nextInt(11) + 10;
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(ALLOWED_CHARACTERS.length());
+            sb.append(ALLOWED_CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         tv = findViewById(R.id.tv);
 
         String randomString = generateRandomString();
 
         double doubleValue = 100.00;
         float floatValue = (float) doubleValue;
-        Toast.makeText(this, ""+floatValue, Toast.LENGTH_SHORT).show();
+
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,15 +64,15 @@ public class MainActivity extends AppCompatActivity {
                 Customer customer = new Customer(
                         "Akalanka",
                         "Kaushalya",
-                        "+94779805664",
+                        "0779805664",
                         "kaushalyaakalanka343@gmail.com"
                 );
 
                 // Set payment configurations (Replace placeholders with actual data)
                 Configurations configurations = new Configurations(
-                        "8945a60756f2ee7c4a9ea7962659a59c12d50b40dbcb889fcd76f9b16616d924fe657cd8517b8d20.OWKA118C243F01B2DB54F",
-                        "RRNJ118C243F01B2DB50B",
-                        "B60R118C243F01B2DB536"
+                        "d6ab21065d748866d05aae2cb35bb0e4e118fce830c6db3b7b13eb8454609ae9e328fd8ca4b3a7b2.ZCLG118C243ED0A26BD1B",
+                        "FCVW118C243ED0A26BCD3",
+                        "72Z6118C243ED0A26BD04"
                 );
 
                 // Set product details (Replace placeholders with actual data)
@@ -94,23 +107,24 @@ public class MainActivity extends AppCompatActivity {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Bundle data = result.getData() != null ? result.getData().getExtras() : null;
                         assert result.getData() != null;
-                        tv.setText(result.getData().toString());
-                        Toast.makeText(MainActivity.this, ""+data, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, ""+data, Toast.LENGTH_SHORT).show();
                         if (data != null) {
-                            //IPGStatus status = (IPGStatus) data.getSerializable(OnepayIPG.IPG_RESULT);
+                            String status = (String) data.getSerializable(OnepayIPG.IPG_RESULT);
                             Toast.makeText(MainActivity.this, ""+data.getSerializable(OnepayIPG.IPG_RESULT), Toast.LENGTH_SHORT).show();
-                            //data.getSerializable(OnepayIPG.IPG_RESULT);
-//                            if (status == IPGStatus.SUCCESS) {
-//                                OnepayIPGSuccess resultData = (OnepayIPGSuccess) data.getSerializable(OnepayIPG.IPG_RESULT_DATA);
-//                                if (resultData != null) {
-//                                    Toast.makeText(MainActivity.this, resultData.getDescription(), Toast.LENGTH_LONG).show();
-//                                }
-//                            } else if (status == IPGStatus.FAILED) {
-//                                OnepayIPGError resultData = (OnepayIPGError) data.getSerializable(OnepayIPG.IPG_RESULT_DATA);
-//                                if (resultData != null) {
-//                                    Toast.makeText(MainActivity.this, resultData.getDescription(), Toast.LENGTH_LONG).show();
-//                                }
-//                            }
+                            Toast.makeText(MainActivity.this, ""+data.getSerializable(OnepayIPG.IPG_DATA), Toast.LENGTH_SHORT).show();
+                            tv.setText(OnepayIPG.IPG_DATA);
+                            data.getSerializable(OnepayIPG.IPG_RESULT);
+                            if (status == "SUCCESS") {
+                                OnepayIPGSuccess resultData = (OnepayIPGSuccess) data.getSerializable(OnepayIPG.IPG_RESULT_DATA);
+                                if (resultData != null) {
+                                    Toast.makeText(MainActivity.this, resultData.getDescription(), Toast.LENGTH_LONG).show();
+                                }
+                            } else if (status == "FAILED") {
+                                OnepayIPGError resultData = (OnepayIPGError) data.getSerializable(OnepayIPG.IPG_RESULT_DATA);
+                                if (resultData != null) {
+                                    Toast.makeText(MainActivity.this, resultData.getDescription(), Toast.LENGTH_LONG).show();
+                                }
+                            }
                             // Handle other cases if needed
                         }
                     }
@@ -118,20 +132,7 @@ public class MainActivity extends AppCompatActivity {
             }
     );
 
-    private static final String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    public static String generateRandomString() {
-        SecureRandom random = new SecureRandom();
-        // Generate a random length between 10 and 20
-        int length = random.nextInt(11) + 10;
-        // Build the random string
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(ALLOWED_CHARACTERS.length());
-            sb.append(ALLOWED_CHARACTERS.charAt(randomIndex));
-        }
 
-        return sb.toString();
-    }
 
 
 }
